b9908d432126e66f66e81290cb1789da30dc9028
Lon Bitner <lonbitner@gmail.com>
2020-04-21 09:18:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,4 +2,6 @@ reverse_each_word = ["Hello there, and how are you?"]
 
 def reverse(reverse_each_word)
   reverse_each_word.each do |reverse_each_word|
-    puts
+    puts "Hello there, and how are you?".reverse
+  end
+end 
